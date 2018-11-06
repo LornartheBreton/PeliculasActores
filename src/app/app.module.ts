a0b174f0915e9b59941aecd61e55b3c1b7f98cf4
Leonardo Briseño <hectorspacejam@gmail.com>
2018-11-06 08:08:24 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { ActorsPage } from '../pages/actors/actors';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ActorsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +20,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ActorsPage
   ],
   providers: [
     StatusBar,
